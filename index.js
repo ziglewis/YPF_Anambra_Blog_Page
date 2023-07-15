e77b5ea5eeeb9c7e0f1cae57715bc7f9646775ed
ziglewis <ziglewis48@gmail.com>
2023-07-15 20:29:36 +0100

    d

--- index.js
+++ index.js
@@ -228,5 +228,14 @@ let myBlogs = document.querySelectorAll(".each-particular-blog-visible-content")
 
     })
 
+    window.onpopstate = function(event) {
+        // Check if the URL has a hash.
+        if (!window.location.hash) {
+          // If not, add a hash and reload the page.
+          window.location = window.location + '#loaded';
+          window.location.reload();
+        }
+      };
+
 
       
